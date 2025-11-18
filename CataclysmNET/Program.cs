af8550590b7e77fbac63c081eb2a82afd1ebc8c7
Ovahlord <18347559+Ovahlord@users.noreply.github.com>
2025-11-18 20:13:14 +0100

    Set up empty base classes for Login, Realm and World services

--- CataclysmNET/Program.cs
+++ CataclysmNET/Program.cs
@@ -1,1 +1,13 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using CataclysmNET.Services;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+HostApplicationBuilder builder = Host.CreateApplicationBuilder();
+
+builder.Services.AddHostedService<LoginService>();
+builder.Services.AddHostedService<RealmService>();
+builder.Services.AddHostedService<WorldService>();
+
+using IHost host = builder.Build();
+
+await host.RunAsync();
---
+++ CataclysmNET/Services/WorldService.cs
@@ -0,0 +1,17 @@
+using Microsoft.Extensions.Hosting;
+
+namespace CataclysmNET.Services
+{
+    public class WorldService : IHostedService
+    {
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
